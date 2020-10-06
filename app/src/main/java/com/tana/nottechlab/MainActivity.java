57f18d1c1aa997ad244a7e89d104b0deae053ee0
tana <charleatanasi@gmail.com>
2020-10-06 14:52:20 +0300

    AddMemberActivity And Store data to database

--- app/src/main/java/com/tana/nottechlab/MainActivity.java
+++ app/src/main/java/com/tana/nottechlab/MainActivity.java
@@ -1,8 +1,13 @@
 package com.tana.nottechlab;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent addMember = new Intent(MainActivity.this, AddMemberActivity.class);
+                startActivityForResult(addMember, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
